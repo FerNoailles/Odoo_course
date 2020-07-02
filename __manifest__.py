5f442b56cfd7617fbb78bb7b40fbc7e9e800fbbc
vagrant <vagrant@vagrant.vm>
2020-07-02 08:12:55 +0000

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Helpdesk Factor Libre',
+    'summary': "Manage systems from helpdesk course",
+    'version': '11.0.1.0.1',
+    'author': "Fernando Vasoncellos",
+    'license': "AGPL-3",
+    'maintainer': 'Fernando Vasconcellos',
+    'category': 'Helpdesk',
+    'website': 'https://odoo-community.org/',
+    'depends': ['mail'],
+    'data':[
+        'views/helpdesk_ticket_views.xml',
+        'views/helpdesk_ticket_tag_views.xml',
+        'views/inherit_res_partner_views.xml',
+        'Security/helpdesk_security.xml',
+        ],
+    'installable': True,
+}
