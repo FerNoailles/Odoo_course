15907e7a24cd2ac2d21cd8b7c2dfdae54eed7c07
Fernando <fer.gvn18@gmail.com>
2020-07-08 19:02:33 +0200

    cleaning repositories

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Fernando Vasconcellos
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import helpdesk_ticket
-from . import helpdesk_ticket_tag
-from . import inherit_res_partner
